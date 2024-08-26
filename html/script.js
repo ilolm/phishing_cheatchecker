7ca1d3ab08b2c145f28a1da4d7c219b6bb7be06e
Ilolm <kolya.ogorodnik.roma@gmail.com>
2024-08-26 21:38:49 +0000

    First commit.

---
+++ html/script.js
@@ -0,0 +1,11 @@
+// For smooth scrolling
+
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
